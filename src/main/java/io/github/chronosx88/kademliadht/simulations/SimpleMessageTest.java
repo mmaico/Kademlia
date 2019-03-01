d7091133735b2d226dc53517e8862d954b798de2
ChronosX88 <chronosx88@gmail.com>
2019-03-01 20:37:27 +0400

    Migrate project to Gradle, as library. Fixed hex representation of NodeID.

---
+++ src/main/java/io/github/chronosx88/kademliadht/simulations/SimpleMessageTest.java
@@ -0,0 +1,32 @@
+package io.github.chronosx88.kademliadht.simulations;
+
+import java.io.IOException;
+import io.github.chronosx88.kademliadht.JKademliaNode;
+import io.github.chronosx88.kademliadht.message.SimpleMessage;
+import io.github.chronosx88.kademliadht.node.KademliaId;
+import io.github.chronosx88.kademliadht.message.SimpleReceiver;
+
+/**
+ * Test 1: Try sending a simple message between nodes
+ *
+ * @author Joshua Kissoon
+ * @created 20140218
+ */
+public class SimpleMessageTest
+{
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            JKademliaNode kad1 = new JKademliaNode("Joshua", new KademliaId("12345678901234567890"), 7574);
+            JKademliaNode kad2 = new JKademliaNode("Crystal", new KademliaId("12345678901234567891"), 7572);
+
+            kad1.getServer().sendMessage(kad2.getNode(), new SimpleMessage("Some Message"), new SimpleReceiver());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
